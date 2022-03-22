2c05ba4b6c8fd84207bd1665210a17a7f1f5bc26
George <gsheslow@yahoo.com.au>
2022-03-22 11:38:12 +1100

    Basic GUI, crude arm movements with slider

--- fetch_robot_sim/scripts/gui/gui.py
+++ fetch_robot_sim/scripts/gui/gui.py
@@ -16,7 +16,7 @@ from PyQt5 import uic
 from teleop import TeleOp
 from cameras import Cameras
 from sim.fetch import Robot
-
+from sim.teleop_twist_keyboard import PublishThread
 #from scripts.sim.fetch import Robot
 
 class GUI(QWidget):
@@ -28,29 +28,34 @@ class GUI(QWidget):
         self.cameras = Cameras()
         self.teleop = TeleOp()
 
-        self.fetch = Robot()
+        self.fetch = Robot("Fetchy")
+
+        self.robot_move_control = PublishThread(0)
 
         self.layout.addWidget(self.cameras)
         self.layout.addWidget(self.teleop)
 
         self.show()
+        
+        self.Arm_Reset_Button.clicked.connect(self.robo_arm_reset)
+        self.teleop.J1_Slider.sliderReleased.connect(self.robo_J1_update)
+        self.teleop.J2_Slider.sliderReleased.connect(self.robo_J2_update)
+        self.teleop.J3_Slider.sliderReleased.connect(self.robo_J3_update)
+        self.teleop.J4_Slider.sliderReleased.connect(self.robo_J4_update)
+        self.teleop.J5_Slider.sliderReleased.connect(self.robo_J5_update)
+        self.teleop.J6_Slider.sliderReleased.connect(self.robo_J6_update)
+        self.teleop.J7_Slider.sliderReleased.connect(self.robo_J7_update)
 
-        self.teleop.J1_Slider.valueChanged.connect(self.robo_J1_update)
-        self.teleop.J2_Slider.valueChanged.connect(self.robo_J2_update)
-        self.teleop.J3_Slider.valueChanged.connect(self.robo_J3_update)
-        self.teleop.J4_Slider.valueChanged.connect(self.robo_J4_update)
-        self.teleop.J5_Slider.valueChanged.connect(self.robo_J5_update)
-        self.teleop.J6_Slider.valueChanged.connect(self.robo_J6_update)
-        self.teleop.J7_Slider.valueChanged.connect(self.robo_J7_update)
-
-        self.teleop.Head_Slider.valueChanged.connect(self.robo_head_update)
-        self.teleop.Bellow_Slider.valueChanged.connect(self.robo_bellow_update)
-        self.teleop.Gripper_Slider.valueChanged.connect(self.robo_gripper_update)
+        self.teleop.Head_Slider.sliderReleased.connect(self.robo_head_update)
+        self.teleop.Bellow_Slider.sliderReleased.connect(self.robo_bellow_update)
+        self.teleop.Gripper_Slider.sliderReleased.connect(self.robo_gripper_update)
 
         self.teleop.Forward_Button.clicked.connect(self.robo_move_forward)
         self.teleop.Back_Button.clicked.connect(self.robo_move_back)
         # Add rotation buttons and commands
 
+    def robo_arm_reset(self):
+        self.fetch.reset_arm()
 
     # values from slider goes from int 0 -> 99
     def robo_J1_update(self):
@@ -59,22 +64,22 @@ class GUI(QWidget):
         self.fetch.update_arm_joints(0,self.teleop.J1_Slider.value())
 
     def robo_J2_update(self):
-        self.fetch.update_arm_joints(0,self.teleop.J2_Slider.value())
+        self.fetch.update_arm_joints(1,self.teleop.J2_Slider.value())
 
     def robo_J3_update(self):
-        self.fetch.update_arm_joints(0,self.teleop.J3_Slider.value())
+        self.fetch.update_arm_joints(2,self.teleop.J3_Slider.value())
 
     def robo_J4_update(self):
-        self.fetch.update_arm_joints(0,self.teleop.J4_Slider.value())
+        self.fetch.update_arm_joints(3,self.teleop.J4_Slider.value())
 
     def robo_J5_update(self):
-        self.fetch.update_arm_joints(0,self.teleop.J5_Slider.value())
+        self.fetch.update_arm_joints(4,self.teleop.J5_Slider.value())
 
     def robo_J6_update(self):
-        self.fetch.update_arm_joints(0,self.teleop.J6_Slider.value())
+        self.fetch.update_arm_joints(5,self.teleop.J6_Slider.value())
 
     def robo_J7_update(self):
-        self.fetch.update_arm_joints(0,self.teleop.J7_Slider.value())
+        self.fetch.update_arm_joints(6,self.teleop.J7_Slider.value())
 
     def robo_head_update(self):
         pass
--- fetch_robot_sim/scripts/gui/sim/fetch.py
+++ fetch_robot_sim/scripts/gui/sim/fetch.py
@@ -59,6 +59,27 @@ class Robot():
         self.gripper_client.wait_for_server()
         rospy.loginfo("...connected.")
 
+        rospy.loginfo("Waiting for torso_controller...")
+        self.arm_client = actionlib.SimpleActionClient("torso_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+        self.arm_client.wait_for_server()
+        rospy.loginfo("...connected.")
+
+    def setup_torso(self):
+        trajectory = JointTrajectory()
+        trajectory.joint_names = self.head_joint_names
+        trajectory.points.append(JointTrajectoryPoint())
+        trajectory.points[0].positions = self.head_joint_positions
+        trajectory.points[0].velocities = [0.0] * len(self.head_joint_positions)
+        trajectory.points[0].accelerations = [0.0] * len(self.head_joint_positions)
+        trajectory.points[0].time_from_start = rospy.Duration(5.0)
+
+        head_goal = FollowJointTrajectoryGoal()
+        head_goal.trajectory = trajectory
+        head_goal.goal_time_tolerance = rospy.Duration(0.0)
+
+        rospy.loginfo("Setting positions...")
+        self.head_client.send_goal(head_goal)
+        self.head_client.wait_for_result(rospy.Duration(1.0))        
 
     def setup_head(self):
         trajectory = JointTrajectory()
@@ -80,11 +101,32 @@ class Robot():
 
     def setup_arm(self):
         pass
+
+    def reset_arm(self):
+        self.joint_positions = self.joint_pre_grasp_position
+        self.execute_arm_update()
     
     def update_arm_joints(self, arm_joint_num, pos):
-        # change pos from 0 -> 99 to -Pi -> Pi
-        value = pos * math.pi
-        self.joint_positions[arm_joint_num+1] = value
+        # map slider pos from 0 -> 9 to joint min and max
+        pos = pos / 100
+        if (arm_joint_num == 0):
+            value = pos * math.pi /2
+        if (arm_joint_num == 1):
+            value = pos * math.pi /2
+        if (arm_joint_num == 2):
+            value = pos * math.pi /2
+        if (arm_joint_num == 3):
+            value = pos * math.pi /2
+        if (arm_joint_num == 4):
+            value = pos * math.pi /2
+        if (arm_joint_num == 5):
+            value = pos * math.pi /2
+        if (arm_joint_num == 6):
+            value = pos * math.pi /2
+        if (arm_joint_num == 7):
+            value = pos * math.pi /2
+
+        self.joint_positions[arm_joint_num] = value
         self.execute_arm_update()
 
 
@@ -109,8 +151,8 @@ class Robot():
         trajectory.joint_names = self.arm_joint_names
         trajectory.points.append(JointTrajectoryPoint())
         trajectory.points[0].positions = self.joint_positions
-        trajectory.points[0].velocities =  [0.0] * len(self.arm_joint_positions)
-        trajectory.points[0].accelerations = [0.0] * len(self.arm_joint_positions)
+        trajectory.points[0].velocities =  [0.0] * len(self.joint_positions)
+        trajectory.points[0].accelerations = [0.0] * len(self.joint_positions)
         trajectory.points[0].time_from_start = rospy.Duration(1.0)
 
         arm_goal = FollowJointTrajectoryGoal()
@@ -119,7 +161,7 @@ class Robot():
 
 
         self.arm_client.send_goal(arm_goal)
-        self.arm_client.wait_for_result(rospy.Duration(10.0))
+        self.arm_client.wait_for_result(rospy.Duration(1.0))
 
     def execute_head_update(self):
         pass
@@ -129,3 +171,9 @@ class Robot():
 
     def execute_gripper_update(self):
         pass
+
+
+
+    def get_remapped_arm_joints(self):
+        pass
+        
