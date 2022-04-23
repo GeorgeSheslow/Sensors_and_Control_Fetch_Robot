7354b18e3eba5cfe76f1ff32c74a0a219e89e11c
Vanesssa Leung <13228869@student.uts.edu.au>
2022-04-23 10:03:34 +1000

    Cmake fixes

--- iksolver/src/main.cpp
+++ iksolver/src/main.cpp
@@ -1,3 +1,39 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2012, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Sachin Chitta, Michael Lautman */
+
 #include <pluginlib/class_loader.h>
 #include <ros/ros.h>
 
@@ -14,13 +50,26 @@
 
 int main(int argc, char** argv)
 {
-  const std::string node_name = "iksolver";
+  const std::string node_name = "motion_planning_tutorial";
   ros::init(argc, argv, node_name);
   ros::AsyncSpinner spinner(1);
   spinner.start();
   ros::NodeHandle node_handle("~");
 
-  //Setup planar
+  // BEGIN_TUTORIAL
+  // Start
+  // ^^^^^
+  // Setting up to start using a planner is pretty easy. Planners are
+  // setup as plugins in MoveIt and you can use the ROS pluginlib
+  // interface to load any planner that you want to use. Before we
+  // can load the planner, we need two objects, a RobotModel and a
+  // PlanningScene. We will start by instantiating a `RobotModelLoader`_
+  // object, which will look up the robot description on the ROS
+  // parameter server and construct a :moveit_core:`RobotModel` for us
+  // to use.
+  //
+  // .. _RobotModelLoader:
+  //     http://docs.ros.org/noetic/api/moveit_ros_planning/html/classrobot__model__loader_1_1RobotModelLoader.html
   const std::string PLANNING_GROUP = "panda_arm";
   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
   const moveit::core::RobotModelPtr& robot_model = robot_model_loader.getModel();
@@ -28,18 +77,22 @@ int main(int argc, char** argv)
   moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(robot_model));
   const moveit::core::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
 
-  // Using: moveit_core:`RobotModel`, and planning_scene: `PlanningScene`, creates a planning scene (including the robot).
+  // Using the :moveit_core:`RobotModel`, we can construct a :planning_scene:`PlanningScene`
+  // that maintains the state of the world (including the robot).
   planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
 
   // Configure a valid robot state
   planning_scene->getCurrentStateNonConst().setToDefaultValues(joint_model_group, "ready");
 
-  // We will now construct a loader to load a planner, by name. Using the ROS pluginlib library.
+  // We will now construct a loader to load a planner, by name.
+  // Note that we are using the ROS pluginlib library here.
   boost::scoped_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager>> planner_plugin_loader;
   planning_interface::PlannerManagerPtr planner_instance;
   std::string planner_plugin_name;
 
-  // We will get the name of planning plugin we want to load from the ROS parameter server, and then load the planner making sure to catch all exceptions.
+  // We will get the name of planning plugin we want to load
+  // from the ROS parameter server, and then load the planner
+  // making sure to catch all exceptions.
   if (!node_handle.getParam("planning_plugin", planner_plugin_name))
     ROS_FATAL_STREAM("Could not find planner plugin name");
   try
@@ -64,12 +117,14 @@ int main(int argc, char** argv)
     std::stringstream ss;
     for (const auto& cls : classes)
       ss << cls << " ";
-    ROS_ERROR_STREAM("Exception while loading planner '" << planner_plugin_name << "': " << ex.what() << std::endl << "Available plugins: " << ss.str());
+    ROS_ERROR_STREAM("Exception while loading planner '" << planner_plugin_name << "': " << ex.what() << std::endl
+                                                         << "Available plugins: " << ss.str());
   }
 
   // Visualization
   // ^^^^^^^^^^^^^
-  // MoveItVisualTools provides many capabilities for visualizing objects, robots, and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script.
+  // The package MoveItVisualTools provides many capabilities for visualizing objects, robots,
+  // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script.
   namespace rvt = rviz_visual_tools;
   moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
   visual_tools.loadRobotStatePub("/display_robot_state");
@@ -77,7 +132,8 @@ int main(int argc, char** argv)
   visual_tools.deleteAllMarkers();  // clear all old markers
   visual_tools.trigger();
 
-  /* Remote control is an introspection tool that allows users to step through a high level script via buttons and keyboard shortcuts in RViz */
+  /* Remote control is an introspection tool that allows users to step through a high level script
+     via buttons and keyboard shortcuts in RViz */
   visual_tools.loadRemoteControl();
 
   /* RViz provides many types of markers, in this demo we will use text, cylinders, and spheres*/
@@ -88,12 +144,13 @@ int main(int argc, char** argv)
   /* Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations */
   visual_tools.trigger();
 
-  /*Visual_tools to wait for user input */
+  /* We can also use visual_tools to wait for user input */
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
 
   // Pose Goal
   // ^^^^^^^^^
-  // Motion plan request for the arm of the Panda specifying the desired pose of the end-effector as input.
+  // We will now create a motion plan request for the arm of the Panda
+  // specifying the desired pose of the end-effector as input.
   visual_tools.publishRobotState(planning_scene->getCurrentStateNonConst(), rviz_visual_tools::GREEN);
   visual_tools.trigger();
   planning_interface::MotionPlanRequest req;
@@ -105,18 +162,29 @@ int main(int argc, char** argv)
   pose.pose.position.z = 0.75;
   pose.pose.orientation.w = 1.0;
 
-  // A tolerance of 0.01 m is specified in position and 0.01 radians in orientation
+  // A tolerance of 0.01 m is specified in position
+  // and 0.01 radians in orientation
   std::vector<double> tolerance_pose(3, 0.01);
   std::vector<double> tolerance_angle(3, 0.01);
 
-  // Kinematics constraints
-  moveit_msgs::Constraints pose_goal = kinematic_constraints::constructGoalConstraints("panda_link8", pose, tolerance_pose, tolerance_angle);
+  // We will create the request as a constraint using a helper function available
+  // from the
+  // `kinematic_constraints`_
+  // package.
+  //
+  // .. _kinematic_constraints:
+  //     http://docs.ros.org/noetic/api/moveit_core/html/cpp/namespacekinematic__constraints.html#a88becba14be9ced36fefc7980271e132
+  moveit_msgs::Constraints pose_goal =
+      kinematic_constraints::constructGoalConstraints("panda_link8", pose, tolerance_pose, tolerance_angle);
 
   req.group_name = PLANNING_GROUP;
   req.goal_constraints.push_back(pose_goal);
 
-  // We now construct a planning context that encapsulate the scene, the request and the response. We call the planner using this planning context
-  planning_interface::PlanningContextPtr context = planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
+  // We now construct a planning context that encapsulate the scene,
+  // the request and the response. We call the planner using this
+  // planning context
+  planning_interface::PlanningContextPtr context =
+      planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
   context->solve(res);
   if (res.error_code_.val != res.error_code_.SUCCESS)
   {
@@ -126,7 +194,8 @@ int main(int argc, char** argv)
 
   // Visualize the result
   // ^^^^^^^^^^^^^^^^^^^^
-  ros::Publisher display_publisher =  node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+  ros::Publisher display_publisher =
+      node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
   moveit_msgs::DisplayTrajectory display_trajectory;
 
   /* Visualize the trajectory */
@@ -153,7 +222,8 @@ int main(int argc, char** argv)
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
   // Joint Space Goals
-  // Setup a joint space goal
+  // ^^^^^^^^^^^^^^^^^
+  // Now, setup a joint space goal
   moveit::core::RobotState goal_state(robot_model);
   std::vector<double> joint_values = { -1.0, 0.7, 0.7, -1.5, -0.7, 2.0, 0.0 };
   goal_state.setJointGroupPositions(joint_model_group, joint_values);
@@ -181,7 +251,8 @@ int main(int argc, char** argv)
   visual_tools.trigger();
   display_publisher.publish(display_trajectory);
 
-  /* We will add more goals. But first, set the state in the planning scene to the final state of the last plan */
+  /* We will add more goals. But first, set the state in the planning
+     scene to the final state of the last plan */
   robot_state->setJointGroupPositions(joint_model_group, response.trajectory.joint_trajectory.points.back().positions);
   planning_scene->setCurrentState(*robot_state.get());
 
@@ -218,27 +289,35 @@ int main(int argc, char** argv)
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
   // Adding Path Constraints
+  // ^^^^^^^^^^^^^^^^^^^^^^^
   // Let's add a new pose goal again. This time we will also add a path constraint to the motion.
-  /* Create a new pose goal */
+  /* Let's create a new pose goal */
+
   pose.pose.position.x = 0.32;
   pose.pose.position.y = -0.25;
   pose.pose.position.z = 0.65;
   pose.pose.orientation.w = 1.0;
-  moveit_msgs::Constraints pose_goal_2 = kinematic_constraints::constructGoalConstraints("panda_link8", pose, tolerance_pose, tolerance_angle);
+  moveit_msgs::Constraints pose_goal_2 =
+      kinematic_constraints::constructGoalConstraints("panda_link8", pose, tolerance_pose, tolerance_angle);
 
   /* Now, let's try to move to this new pose goal*/
   req.goal_constraints.clear();
   req.goal_constraints.push_back(pose_goal_2);
 
-  /* But, let's impose a path constraint on the motion. Asking for the end-effector to stay level*/
+  /* But, let's impose a path constraint on the motion.
+     Here, we are asking for the end-effector to stay level*/
   geometry_msgs::QuaternionStamped quaternion;
   quaternion.header.frame_id = "panda_link0";
   quaternion.quaternion.w = 1.0;
   req.path_constraints = kinematic_constraints::constructGoalConstraints("panda_link8", quaternion);
 
-  // Imposing path constraints requires the planner to reason in the space of possible positions of the end-effector (the workspace of the robot)
+  // Imposing path constraints requires the planner to reason in the space of possible positions of the end-effector
+  // (the workspace of the robot)
   // because of this, we need to specify a bound for the allowed planning volume as well;
-  // We use a bound that definitely includes the reachable space for the arm. This is fine because sampling is not done in this volume
+  // Note: a default bound is automatically filled by the WorkspaceBounds request adapter (part of the OMPL pipeline,
+  // but that is not being used in this example).
+  // We use a bound that definitely includes the reachable space for the arm. This is fine because sampling is not done
+  // in this volume
   // when planning for the arm; the bounds are only used to determine if the sampled configurations are valid.
   req.workspace_parameters.min_corner.x = req.workspace_parameters.min_corner.y =
       req.workspace_parameters.min_corner.z = -2.0;
@@ -264,6 +343,7 @@ int main(int argc, char** argv)
   visual_tools.publishText(text_pose, "Orientation Constrained Motion Plan (3)", rvt::WHITE, rvt::XLARGE);
   visual_tools.trigger();
 
+  // END_TUTORIAL
   /* Wait for user input */
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to exit the demo");
   planner_instance.reset();
