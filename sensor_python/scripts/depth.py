f13d4b6c47e44eae7a7e1236a6b582d961108748
PJ-Un <82499541+PJ-Un@users.noreply.github.com>
2022-04-27 02:00:11 +1000

    Name Changes

--- sensor_python/scripts/depth.py
+++ sensor_python/scripts/depth.py
@@ -10,7 +10,7 @@ from cv_bridge import CvBridge, CvBridgeError
 #from geometry_msgs.msg import Twist
 from msg import Object_Info
 from msg import Location
-from msg import 3DLocation
+from msg import Location_3D
 
 class Depth_Detection:
 
@@ -22,7 +22,7 @@ class Depth_Detection:
         self.location_sub =rospy.Subscriber("Point_Center",Location,self.cameraDepthCallBack)
         
         self.detect_object = rospy.Publisher("object_info", Object_Info, queue_size=10)
-        self.location_3D = rospy.Publisher("distance",3DLocation, queue_size=10)
+        self.location_3D = rospy.Publisher("distance",Location_3D, queue_size=10)
 
     def cameraDepthCallBack(self,data):
         try:
