1ffc680de68c8dd3b7af99a6409af31008696da2
PJ-Un <pj1998paul@gmail.com>
2022-04-29 23:27:08 -0700

    edits to code (publish work in progress)

--- sensor_python/scripts/sensor.py
+++ sensor_python/scripts/sensor.py
@@ -9,7 +9,7 @@ import numpy as np
 from sensor_msgs.msg import Image
 from std_msgs.msg import String, Float64MultiArray, Float32
 from cv_bridge import CvBridge, CvBridgeError
-from msg import Location
+#from msg import Location
 
 # Create an object to read camera video 
 class RGB_Detection:
@@ -18,17 +18,17 @@ class RGB_Detection:
         self.bridge_object = CvBridge()
         self.image_sub =rospy.Subscriber("/head_camera/rgb/image_raw",Image,self.cameraRGBCallBack)
         
-        self.pointCentre = rospy.Publisher("point_center", Location,queue_size = 10)
+    #    self.pointCentre = rospy.Publisher("point_center", Location,queue_size = 10)
         
 
     def cameraRGBCallBack(self,data):
         try:
             cap = self.bridge_object.imgmsg_to_cv2(data,"bgr8")
-            ret, frame = cap.read()
-            width = int(cap.get(3))
-            height = int(cap.get(4))
+            #ret, frame = cap.read()
+            # width = int(cap.get(3))
+            # height = int(cap.get(4))
             # Convert BGR to HSV
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            hsv = cv2.cvtColor(cap, cv2.COLOR_BGR2HSV)
             # define blue colour range
             light_blue = np.array([94,80,2], np.uint8) 
             dark_blue = np.array([130,255,255], np.uint8)
@@ -48,7 +48,7 @@ class RGB_Detection:
             dark_green = np.array([102, 255, 255], np.uint8) 
             
             # Threshold the HSV image to get only green colours
-            green_mask = cv2.inRange(frame, light_green, dark_green) 
+            green_mask = cv2.inRange(cap, light_green, dark_green) 
 
 
             kernal = np.ones((5,5), "uint8")
@@ -57,9 +57,9 @@ class RGB_Detection:
             green_mask= cv2.dilate(green_mask, kernal)
             
             # Bitwise-AND mask and original image
-            output_blue = cv2.bitwise_and(frame,frame, mask= blue_mask)
-            output_red = cv2.bitwise_and(frame,frame, mask= red_mask)
-            output_green = cv2.bitwise_and(frame,frame, mask= green_mask)
+            output_blue = cv2.bitwise_and(cap,cap, mask= blue_mask)
+            output_red = cv2.bitwise_and(cap,cap, mask= red_mask)
+            output_green = cv2.bitwise_and(cap,cap, mask= green_mask)
             
             # Creating contour to track red colour 
             contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
@@ -71,12 +71,12 @@ class RGB_Detection:
                 area = cv2.contourArea(contour) 
                 if(area > 300): 
                     x, y, w, h = cv2.boundingRect(contour) 
-                    frame = cv2.rectangle(frame, (x, y),  (x + w, y + h), (0, 0, 255), 2) 
+                    cap = cv2.rectangle(cap, (x, y),  (x + w, y + h), (0, 0, 255), 2) 
                     
                     midPoints = [x+(w/2),y+(h/2)]
-                    self.pointCentre.publish(midPoints)
+    #                self.pointCentre.publish(midPoints)
                     
-                    cv2.putText(frame, "Red Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))	 
+                    cv2.putText(cap, "Red Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))	 
 
             # Creating contour to track green colour 
             contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
@@ -85,12 +85,12 @@ class RGB_Detection:
                 area = cv2.contourArea(contour) 
                 if(area > 300): 
                     x, y, w, h = cv2.boundingRect(contour) 
-                    frame = cv2.rectangle(frame, (x, y), (x + w, y + h),  (0, 255, 0), 2) 
+                    cap = cv2.rectangle(cap, (x, y), (x + w, y + h),  (0, 255, 0), 2) 
                     
                     midPoints = [x+(w/2),y+(h/2)]
-                    self.pointCentre.publish(midPoints)
+        #            self.pointCentre.publish(midPoints)
                 
-                    cv2.putText(frame, "Green Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0)) 
+                    cv2.putText(cap, "Green Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0)) 
 
             # Creating contour to track blue colour 
             contours, hierarchy = cv2.findContours(blue_mask, cv2.RETR_TREE,  cv2.CHAIN_APPROX_SIMPLE) 
@@ -99,15 +99,15 @@ class RGB_Detection:
                 area = cv2.contourArea(contour) 
                 if(area > 300): 
                     x, y, w, h = cv2.boundingRect(contour) 
-                    frame = cv2.rectangle(frame, (x, y),  (x + w, y + h),  (255, 0, 0), 2) 
+                    cap = cv2.rectangle(cap, (x, y),  (x + w, y + h),  (255, 0, 0), 2) 
                     
                     midPoints = [x+(w/2),y+(h/2)]
-                    self.pointCentre.publish(midPoints)
+        #            self.pointCentre.publish(midPoints)
                 
-                    cv2.putText(frame, "Blue Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0)) 
+                    cv2.putText(cap, "Blue Colour", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0)) 
                 
             # Program Termination 
-            cv2.imshow("Multiple Color Detection", frame)                         
+            cv2.imshow("Multiple Color Detection", cap)                         
 
             # Press Q on keyboard to stop recording
             cv2.waitKey(1)
